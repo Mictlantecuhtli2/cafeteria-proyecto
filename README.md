330327072e7c6b65f883f371c6e2b750e1a70493
Eduardo Vargas <eduardovrsmtz@gmail.com>
2023-08-22 15:47:42 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Cafetería (Proyecto)
+
+Presento mi primera incursión en GitHub con el proyecto "Cafetería", una creación sencilla pero estéticamente atractiva. Este proyecto es una práctica del curso: "CSS La Guía Completa - Flexbox, CSS Grid, SASS +20 proyectos".    
+
+## Índice
+
+- [Instalación](#Requisitos #Herramientas #Configuración)
+- [Licencia](#licencia)
+
+## Instalación
+
+Este proyecto se ha desarrollado empleando Gulp con el fin de compilar SASS de manera eficiente. Todas las dependencias esenciales para su correcto funcionamiento se han detallado de manera conveniente en el archivo package.json. Esto asegura un proceso de configuración y ejecución sin obstáculos ni complicaciones. Es importante tener en cuenta que la versión 8.0.0 de la dependencia gulp-imagemin ya no admite "require" y, en cambio, solo es compatible con importaciones de módulos ESM. Si te encuentras con este inconveniente, puedes resolverlo instalando la versión anterior, es decir, la versión 7.1.0 de [ imagemin ], mediante el siguiente comando:
+npm install --save-dev gulp-imagemin@7.1.0
+
+
+
+## Licencia
+
+Garantizo la transparencia de la distribución de este proyecto, detallando la licencia bajo la cual opera. En este caso, he optado por la [Licencia MIT](LICENSE).
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const { src, dest, watch, series, parallel} = require('gulp');
+
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css( done ) {
+    //Compilar sass
+    // Pasos: 1._ Identificar archivo, 2._ Compilarla, 3._ Guardar el .css
+
+    src('src/scss/app.scss')
+        .pipe( sass(  ) )
+        .pipe( postcss( [autoprefixer()]))
+        .pipe( dest('build/css') )
+    
+    done();
+}
+
+function imagenes(done) {
+     src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }))
+        .pipe( dest('build/img'))
+
+    done();
+}
+
+function imgWebp(){
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+}
+
+function imgAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img') )
+}
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
+}
+
+
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+exports.imgWebp = imgWebp;
+exports.imgAvif = imgAvif;
+exports.default = series(imagenes, imgWebp, imgAvif, css, dev);
+
+// series- Se inicia la tarea, y hasta que finaliza, inicia la siguiente tarea
+// parallel- Todas inician al mismo tiempo 
